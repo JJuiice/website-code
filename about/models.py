067ddffa5759f5389956d9135f23bbf0c1170cbf
Ojas Anand <oanand@fastmail.com>
2020-08-27 02:53:01 -0400

    Added Remaining Basic Information to "About"

--- about/models.py
+++ about/models.py
@@ -1,11 +1,42 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
+from datetime import date
+
+current_date = date.today()
+
 
 # Create your models here.
 class TechSkills(models.Model):
-    index = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(5)])
     name = models.CharField(max_length=100)
-    level = models.PositiveSmallIntegerField(default=0)
+    level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(5)])
 
     class Meta:
-        ordering = ['index']
+        ordering = ['-level', 'name']
+
+
+class PE(models.Model):
+    title = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    company = models.CharField(max_length=30)
+    additional_time_info = models.CharField(max_length=30, blank=True)
+    start_period = models.CharField(max_length=10, blank=True)
+    start_year = models.CharField(max_length=4, default=current_date.year)
+    end_period = models.CharField(max_length=10, default="Present")
+    end_year = models.CharField(max_length=4, blank=True)
+    details = models.TextField()
+
+    class Meta:
+        ordering = ['-start_year']
+
+
+class Leadership(models.Model):
+    title = models.CharField(max_length=50)
+    organization = models.CharField(max_length=30)
+    start_period = models.CharField(max_length=10, blank=True)
+    start_year = models.CharField(max_length=4, default=current_date.year)
+    end_period = models.CharField(max_length=10, default="Present")
+    end_year = models.CharField(max_length=4, blank=True)
+    details = models.TextField()
+
+    class Meta:
+        ordering = ['-start_year']
--- about/views.py
+++ about/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from about.models import TechSkills
+from about.models import TechSkills, PE, Leadership
+
 
 # Create your views here.
 def about(request):
     skills = TechSkills.objects.all()
+    pes = PE.objects.all()
+    leaderships = Leadership.objects.all()
     context = {
-        'skills': skills
+        'skills': skills,
+        'pes': pes,
+        'leaderships': leaderships
     }
 
     return render(request, 'about.html', context)
