68aac78bc6c9310fa1a44a6a3d36f8a8d8e92c50
Ojas Anand <oanand@fastmail.com>
2020-08-27 12:07:38 -0400

    Final UI change before release and Added License/Copyright

--- ojasanand/urls.py
+++ ojasanand/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('about/', include("about.urls")),
-    # path('projects/', include("projects.urls"))
 ]
