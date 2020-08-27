351f1ea76d62f938dee86db07bcaea4d4851d08b
Ojas Anand <oanand@fastmail.com>
2020-08-27 16:24:26 -0400

    Fix Deployment Issues

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from about.models import TechSkills, PE, Leadership
+
 
 # Register your models here.
+class TechSkillsAdmin(admin.ModelAdmin):
+    pass
+
+
+class PEAdmin(admin.ModelAdmin):
+    pass
+
+
+class LeadershipAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(TechSkills, TechSkillsAdmin)
+admin.site.register(PE, PEAdmin)
+admin.site.register(Leadership, LeadershipAdmin)
