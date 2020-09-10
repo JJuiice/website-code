8dd5ec573790f121e496f05cb5bbe80610475952
Ojas Anand <oanand@fastmail.com>
2020-09-10 01:33:58 -0400

    Prep for Release

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from projects.models import ProjectList
+
 
 # Register your models here.
+class ProjectListAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ProjectList, ProjectListAdmin)
